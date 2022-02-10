344f4ac445ef08f116a5a5a67856d49accb01a00
Prathibha-stack <99198093+Prathibha-stack@users.noreply.github.com>
2022-02-10 20:51:13 +0530

    indexing

--- practice_string.py
+++ practice_string.py
@@ -12,5 +12,63 @@ str4 = 'python@123'           #collection of alphanumeric and symbols
 conc_str1_and_str2 = str1 + str2
 print("concatination of str1 and str2 is {}".format(conc_str1_and_str2))
 
-# str1 = "collection"
-# str2 = "1234"
+str5 = "collection"
+str6 = "1234"
+str7 = "collection123"
+str8 = "collection$123"
+
+# concatination of string
+
+conc_str5_and_str6 = str5 + str6
+print("concatination of str5 and str6 is {}".format(conc_str5_and_str6))
+
+conc_str7_and_str8 = str7 + str8
+print("concatination of str7 and str8 is{}".format(conc_str7_and_str8))
+
+str10 = "abcd"
+str11 = "98"
+str12 = "abcd98"
+str13 = "abcd#98"
+
+# concationation of string
+
+conc_str10_and_str11 = str10 + str11
+print("concatination of str10 and str11 is{}".format(conc_str10_and_str11))
+
+conc_str12_and_str13 = str12 + str13
+print("concatination of str12 and str13 is{}".format(conc_str12_and_str13))
+
+# indexing of a string
+
+str5 = "Introduction"
+ind_of_d = str5[5]
+ind_of_I = str5[0]
+rev_of_n = str5[-1]
+rev_of_r = str5[-9]
+print("Element in fifth position is {}".format(ind_of_d))
+print("Element in in zeroth position is {}".format(ind_of_I))
+print("Element in -1th position is {}".format(rev_of_n))
+print("Element in -9th position is {}".format(rev_of_r))
+str6 = "Malayalam"
+
+
+# indexing of a string
+
+str1 = "Malayam"
+int_of_a = str1[1]
+int_of_l = str1[2]
+int_of_M = str1[0]
+int_of_o = str1[5]
+int_of_y = str1[4]
+int_of_m = str1[6]
+rev_of_a = str1[-2]
+rev_of_h = str1[-4]
+rev_of_l = str1[-5]
+rev_of_M = str1[-7]
+rev_of_x = str1[-6]
+rev_of_y = str1[-3]
+rev_of_m = str1[-1]
+print("element in first position is {}".format(int_of_a))
+print("element in second position is {}".format(int_of_l))
+
+
